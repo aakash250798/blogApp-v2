d9bab342b582c12c24eea98ffd168d686d6c9dad
aakash250798 <49142838+aakash250798@users.noreply.github.com>
2024-04-12 10:35:46 +0530

    Update WebSpringSecurity.java

--- src/main/java/com/akash/blogApp/config/WebSpringSecurity.java
+++ src/main/java/com/akash/blogApp/config/WebSpringSecurity.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 @EnableWebSecurity
@@ -21,16 +23,27 @@ public class WebSpringSecurity {
 	public WebSpringSecurity (UserDetailsService userDetailsService) {
 		this.userDetailsService=userDetailsService;
 	}
+//	@Bean
+//	public void configure(WebSecurity web) throws Exception {
+//		web.ignoring().anyRequest("/images/**");
+//	    web.ignoring().antMatchers("/images/**", "/js/**", "/css/**", "/webjars/**").;
+//	}
 	@Bean
 	public static PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
+		
+        // other configs...
+    
+     
 		http.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests((authorize)->authorize
+						.requestMatchers("/images/**").permitAll()
 						.requestMatchers(new AntPathRequestMatcher("/showBlog/**")).permitAll()
 						.requestMatchers(new AntPathRequestMatcher("/register/**")).permitAll()
 						.requestMatchers(new AntPathRequestMatcher("/getBlog"))
